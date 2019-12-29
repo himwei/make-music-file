32e45633b13dfefd4ffea0f5440daf2fd42b85b8
himwei <1536322061@qq.com>
2019-12-29 20:21:36 +0800

    make music file

---
+++ Music Production Challenge.php
@@ -0,0 +1,5 @@
+<?php
+    $style = array("Dubstep","Melodic Dubstep","Drum and Bass","Complextro","Future Garage","Happycore","Future Bass","Midtempo");
+    $key = array_rand($style,1);
+    echo $style[$key];
+?>
